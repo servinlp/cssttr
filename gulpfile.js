7ed56104df89d84e23e27cc7a61d60ef11952264
Servin Nissen <servin.nissen@gmail.com>
2018-02-09 13:30:35 +0100

    First week
    Start of grid, fancy nth selector and usable with tabs

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const gulp = require( 'gulp' ),
+	postcss = require( 'gulp-postcss' ),
+	sass = require( 'gulp-sass' ),
+	scsslint = require( 'gulp-scss-lint' ),
+	sourcemaps = require( 'gulp-sourcemaps' ),
+	autoprefixer = require( 'autoprefixer' ),
+	browserSync = require( 'browser-sync' ).create()
+
+gulp.task( 'serveMe', [ 'css' ], () => {
+
+	browserSync.init( {
+		server: {
+		    baseDir: './'
+		},
+		open: false
+	} )
+
+	gulp.watch( [ './scss/*.scss', './scss/**/*.scss' ], [ 'cssLint', 'css' ] )
+	gulp.watch( '*.html' ).on( 'change', browserSync.reload )
+
+})
+
+gulp.task( 'cssLint', () => {
+
+	return gulp.src( [ './scss/*.scss', './scss/**/*.scss' ] )
+		.pipe( scsslint( {
+			config: 'scsslint.yml'
+		} ) )
+
+} )
+
+gulp.task( 'css', () => {
+
+	const plugins = [
+        autoprefixer( { browsers: [ 'last 1 version' ] } )
+        // postcssMergeRules()
+    ]
+
+    return gulp.src( './scss/*.scss' )
+		.pipe( sourcemaps.init() )
+		.pipe( sass().on( 'error', sass.logError ) )
+        .pipe( postcss( plugins ) )
+		.pipe( sourcemaps.write('.') )
+        .pipe(gulp.dest( './' ) )
+		.pipe( browserSync.stream() )
+
+} )
+
+gulp.task( 'watch:css', () => {
+
+	gulp.watch( [ './scss/*.scss', './scss/**/*.scss' ], [ 'cssLint', 'css' ] )
+
+} )
+
+gulp.task( 'default', [ 'cssLint', 'css' ] )
+gulp.task( 'watch', [ 'watch:css' ] )
+gulp.task( 'serve', [ 'serveMe' ] )
